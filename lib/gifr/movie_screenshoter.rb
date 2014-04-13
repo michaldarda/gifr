79ee6b09f38d1312377ff30cd784648ef25763cc
Michał Darda <michaldarda@gmail.com>
2014-04-14 00:10:14 +0200

    Taking screenshots

--- lib/gifr/movie_screenshoter.rb
+++ lib/gifr/movie_screenshoter.rb
@@ -1,11 +1,25 @@
+require 'pathname'
+
 class MovieScreenshoter
+  attr_reader :screenshots
+
   def initialize(movie, options = {})
     @movie = movie
-    @from = options.fetch(:from)
-    @to = options.fetch(:to)
+    @start = options.fetch(:start)
+    @time = Integer(options.fetch(:time))
   end
 
-  def screenshot
-    %x{ ffmpeg -ss #{@from} -i #{@movie} -f image2 -vf fps=fps=1 /tmp/gifr%d.png }
+  def take_screenshots!
+    %x{ ffmpeg -v quiet -ss #{@start} -i #{@movie} -f image2 -vf fps=fps=1 /tmp/gifr_#{Pathname.new(@movie.to_s).basename}_%d.png }
+
+    @screenshots = @time.times.map do |i|
+      "/tmp/gifr_#{Pathname.new(@movie.to_s).basename}_#{i+1}.png"
+    end
+  end
+
+  def cleanup!
+    screenshots.each do |screenshot|
+      %x[ rm -r -f /tmp/#{screenshot} ]
+    end
   end
 end
---
+++ spec/lib/movie_screenshoter_spec.rb
@@ -0,0 +1,46 @@
+require_relative '../../lib/gifr/movie_screenshoter'
+
+describe MovieScreenshoter do
+  it 'takes a screenshots of a movie every 1 second for selected \
+      time period and start time and returns an array of created \
+      screenshots' do
+    movie = Movie.new(filename: 'spec/fixtures/example1.mp4')
+    movie_screenshoter = MovieScreenshoter.new(movie, { start: "00:00:00", time: "6"})
+    movie_screenshoter.take_screenshots!
+
+    expect(movie_screenshoter.screenshots)
+      .to match_array [
+        "/tmp/gifr_example1.mp4_1.png",
+        "/tmp/gifr_example1.mp4_2.png",
+        "/tmp/gifr_example1_mp4_3.png",
+        "/tmp/gifr_example1_mp4_4.png",
+        "/tmp/gifr_example1_mp4_5.png",
+        "/tmp/gifr_example1_mp4_6.png"
+      ]
+  end
+
+  it 'takes a screenshots of a movie every 1 second for selected \
+      time period and start time and takes that screenshots' do
+    movie = Movie.new(filename: 'spec/fixtures/example1.mp4')
+    movie_screenshoter = MovieScreenshoter.new(movie, { start: "00:00:00", time: "6"})
+
+    movie_screenshoter.take_screenshots!
+
+    movie_screenshoter.screenshots.each do |screenshot|
+      expect(File.exists?(screenshot)).to eq true
+    end
+  end
+
+  it 'cleanups after taking screenshots' do
+    movie = Movie.new(filename: 'spec/fixtures/example1.mp4')
+    movie_screenshoter = MovieScreenshoter.new(movie, { start: "00:00:00", time: "6"})
+    movie_screenshoter.take_screenshots!
+
+    movie_screenshoter.cleanup!
+
+    movie_screenshoter.screenshots.each do |screenshot|
+      expect(File.exists?(screenshot)).to eq false
+    end
+  end
+
+end
