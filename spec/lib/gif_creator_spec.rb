d2ba4341e2843fffda161afa922bd56b4a63ee5c
Michał Darda <michaldarda@gmail.com>
2014-04-14 11:20:40 +0200

    One more test

--- spec/lib/gif_creator_spec.rb
+++ spec/lib/gif_creator_spec.rb
@@ -14,4 +14,19 @@ describe GifCreator do
 
     expect(File.exists?("video.gif")).to eq true
   end
+
+   it 'makes a gif from selected videos' do
+    movie = Movie.new(filename: "spec/fixtures/example1.mp4")
+    movie_screenshoter = MovieScreenshoter.new(movie, start: "00:00:00", time: "6")
+
+    movie_screenshoter.take_screenshots!
+    screenshots = movie_screenshoter.screenshots
+
+    output = "spec/fixtures/video.gif"
+
+    gif_creator = GifCreator.new(screenshots, { output: output })
+    gif_creator.make_gif!
+
+    expect(File.exists?(output)).to eq true
+  end
 end
