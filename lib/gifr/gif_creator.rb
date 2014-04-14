0ec05f23f38d61112f14ef7ec294312108eb6eec
Michał Darda <michaldarda@gmail.com>
2014-04-14 11:24:57 +0200

    Setting custom delay

--- lib/gifr/gif_creator.rb
+++ lib/gifr/gif_creator.rb
@@ -5,10 +5,11 @@ class GifCreator
   def initialize(gifs = [], options = {})
     @gifs = ImageList.new(*gifs)
     @output = options.fetch(:output)
+    @delay = options.fetch(:delay) { 20 }
   end
 
   def make_gif!
-    @gifs.delay = 20
+    @gifs.delay = @delay
     @gifs.write(@output)
   end
 end
